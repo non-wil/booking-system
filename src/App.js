f479bffa0116f9b3259bbf9cc126477881821d64
May <nontaya.may@gmail.com>
2019-11-12 21:46:21 +0700

    ✨ image list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import ImageList from 'components/ImageList'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav className="nav">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/img-list">Image List</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/img-list">
+            <ImageList />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ImageList/index.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './style.scss'
+
+function ImageList() {
+  const [imgList, setImgList] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    document.title = `Image List`
+    fetchImageList()
+  }, [])
+
+  const fetchImageList = async () => {
+    setIsLoading(true)
+    await axios
+      .get('https://picsum.photos/v2/list')
+      .then(response => {
+        setIsLoading(false)
+        setImgList(response.data)
+      })
+      .catch(error => {
+        console.log('Cannot fetch image list: ', error)
+      })
+  }
+
+  if (isLoading) return <div>Loading</div>
+  return (
+    <div className="container">
+      <div className="img-list">
+        {imgList.map(img => {
+          return (
+            <div key={img.id} className="img-item">
+              <img src={img.download_url} />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default ImageList
